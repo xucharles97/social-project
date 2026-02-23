d3a61af75def95fc394f346528e853cb7e0bd273
xucharles97 <xucharles97@gmail.com>
2026-02-23 01:21:30 +0000

    Adding CORS handler

--- handler/router.go
+++ handler/router.go
@@ -6,17 +6,18 @@ import (
 	jwtMiddleware "github.com/auth0/go-jwt-middleware"
 
 	jwt "github.com/form3tech-oss/jwt-go"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
-func InitRouter() *mux.Router {
+func InitRouter() http.Handler {
 
 	jwtMiddleware := jwtMiddleware.New(jwtMiddleware.Options{
        ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
            return []byte(mySigningKey), nil
        },
        SigningMethod: jwt.SigningMethodHS256,
-   })
+   	})
 
 	router := mux.NewRouter()
 	router.Handle("/upload", jwtMiddleware.Handler(http.HandlerFunc(uploadHandler))).Methods("POST")
@@ -24,5 +25,10 @@ func InitRouter() *mux.Router {
 
 	router.Handle("/signup", http.HandlerFunc(signUpHandler)).Methods("POST")
 	router.Handle("/signin", http.HandlerFunc(signinHandler)).Methods("POST")
-	return router
+
+    originsOk := handlers.AllowedOrigins([]string{"*"})
+    headersOk := handlers.AllowedHeaders([]string{"Authorization", "Content-Type"})
+    methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "DELETE"})
+
+    return handlers.CORS(originsOk, headersOk, methodsOk)(router)
 }

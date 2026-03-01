a9d31890e8da51a66138b8793ff66a6855a75af8
Charles Xu <xucharles97@gmail.com>
2026-02-28 19:10:03 -0500

    Adding reg page

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,7 +1,123 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Input, Button, message } from "antd";
+import axios from "axios";
 
-function Collection(props) {
-  return <div>Collection</div>;
+import { BASE_URL } from "../constants";
+
+// (mobile) responsiveness design
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 16,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+function Register(props) {
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+
+  const onFinish = (values) => {
+    const { username, password } = values;
+    const option = {
+      method: "POST",
+      url: `${BASE_URL}/signup`,
+      data: {
+        username,
+        password,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    axios(option)
+      .then((res) => {
+        if (res.status === 200) {
+          message.success("registration succeeded!");
+          navigate("/login");
+        }
+      })
+      .catch(() => {
+        message.error("something went wrong, try again later");
+      });
+  };
+
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      className="register"
+    >
+      <Form.Item
+        name="username"
+        label="Username"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={["password"]}
+        rules={[
+          { required: true, message: "Please confirm your password!" },
+          ({ getFieldValue }) => {
+            return {
+              validator(rules, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject("Passwords do not match");
+              },
+            };
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button
+          type="primary"
+          htmlType="submit"
+          className="register-btn"
+          style={{ backgroundColor: "black" }}
+        >
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
 
-export default Collection;
+export default Register;

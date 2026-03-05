25bd09222bf57c950a6df304febcff899aadfd05
Charles Xu <xucharles97@gmail.com>
2026-03-05 01:34:14 -0500

    Adding Post form

---
+++ frontend/src/components/PostForm.js
@@ -0,0 +1,49 @@
+import React, { forwardRef } from "react";
+import { Form, Upload, Input } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+
+export const PostForm = forwardRef(() => {
+  const formItemLayout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 14 },
+  };
+
+  const normFile = (e) => (Array.isArray(e) ? e : e?.fileLIst);
+
+  return (
+    <Form name="validate_other" {...formItemLayout} ref={formRef}>
+      <Form.Item
+        name="description"
+        label="Message"
+        rules={[
+          {
+            required: true,
+            message: "Please input your message",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Dragger">
+        <Form.Item
+          name="uploadPost"
+          valuePropName="fileList"
+          getValueFrom
+          Event={normFile}
+          noStyle
+          rules={[{ required: true, message: "Please input your message" }]}
+        >
+          <Upload.Dragger name="files" beforeUpload={() => false}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+          </Upload.Dragger>
+        </Form.Item>
+      </Form.Item>
+    </Form>
+  );
+});
